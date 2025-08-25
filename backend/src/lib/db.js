2a3cc6f4c11ba81c82ce74aa0ad1b80b7ba40e8e
Prakash <prakashselvaraj986@gmail.com>
2025-08-25 17:03:35 +0530

    Delete backend directory

--- backend/src/lib/db.js
+++
@@ -1,2 +0,0 @@
-import mongoose from 'mongoose'
-export async function connectDb(uri){ mongoose.set('strictQuery', true); return mongoose.connect(uri) }
--- backend/src/routes/config.js
+++
@@ -1,4 +0,0 @@
-import express from 'express'
-const r = express.Router()
-r.get('/', (req,res)=> res.json({ appName:'Virtual SmartGrid', defaultTheme:'dark'}))
-export default r
